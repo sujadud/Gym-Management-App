a056b49175eae762808f2aa11ad3dd99d42d80ce
Sujad Ud Doula <sujad.ud@outlook.com>
2024-06-07 16:41:32 +0600

    done

--- src/GymManagement.Application/obj/Debug/net7.0/GymManagement.Application.AssemblyInfo.cs
+++ src/GymManagement.Application/obj/Debug/net7.0/GymManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc4b141cfd4872ee75072018e4a6788516d41e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8961c856bbbe46d45c1d59dcd2a964c89bb0961e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
