deccf23822457d1656d4525dbb94332fb48a764b
Sujad Ud Doula <sujad.ud@outlook.com>
2024-06-08 23:40:42 +0600

    recap done

--- src/GymManagement.Infrastructure/obj/Debug/net7.0/GymManagement.Infrastructure.AssemblyInfo.cs
+++ src/GymManagement.Infrastructure/obj/Debug/net7.0/GymManagement.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460b704d3d6fe066a4994e16c86cd922ce30d60c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc76dc253e8c730951d86764c64bf43cdf6aa3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
